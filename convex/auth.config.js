1a9f649c6d87a16034c5de6ba54092fc93132fb4
Mohamed Hema <mohamedhema2017@gmail.com>
2023-12-13 15:40:00 +0200

    Add Authentication 🔐

---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://present-mako-38.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
